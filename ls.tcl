283c6d867b10fe8d06e912a1512f530488c97115
Meetali <102135323+meetalik8@users.noreply.github.com>
2023-04-27 09:03:55 +0530

    Add files via upload

---
+++ ls.tcl
@@ -0,0 +1,57 @@
+set ns [new Simulator]
+ 
+$ns rtproto LS
+
+set nf [open lsr.nam w]
+$ns namtrace-all $nf
+
+proc finish {} {
+global ns nf
+$ns flush-trace
+close $nf
+exec nam lsr.nam &
+exit 0
+}
+
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+
+$ns duplex-link $n0 $n1 10Mb 10ms DropTail
+$ns duplex-link $n0 $n3 10Mb 10ms DropTail
+$ns duplex-link $n1 $n2 10Mb 10ms DropTail
+$ns duplex-link $n3 $n2 10Mb 10ms DropTail
+$ns duplex-link $n0 $n2 10Mb 10ms DropTail
+$ns duplex-link $n1 $n3 10Mb 10ms DropTail
+
+$ns duplex-link-op $n0 $n1 orient right
+$ns duplex-link-op $n0 $n3 orient down
+$ns duplex-link-op $n1 $n2 orient down
+$ns duplex-link-op $n3 $n2 orient right
+$ns duplex-link-op $n0 $n2 orient right-down
+$ns duplex-link-op $n1 $n3 orient left-down
+
+set tcp [new Agent/TCP]
+$tcp set class_ 2
+$ns attach-agent $n0 $tcp
+
+set sink [new Agent/TCPSink]
+$ns attach-agent $n2 $sink
+
+$ns connect $tcp $sink
+
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp
+$ftp set type_ FTP
+$ftp set packet_size_ 1000
+$ftp set rate_ 1mb
+
+$ns at 0.1 "$ftp start"
+$ns rtmodel-at 0.3 down $n0 $n2
+$ns rtmodel-at 0.9 up $n0 $n2
+$ns at 1.2 "$ftp stop"
+$ns at 1.5 "finish"
+
+$ns run
+
